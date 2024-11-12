8612ad37c0e8dcd93080ca74b8fe6c3fc0fd3b9c
hibohiboo <hibohiboo66+github@gmail.com>
2024-11-13 07:34:55 +0900

    fix tsconfig #5

--- packages/core/cucumber.mjs
+++ packages/core/cucumber.mjs
@@ -1,6 +1,7 @@
+// https://github.com/cucumber/cucumber-js/blob/main/docs/transpiling.md#esm
+// https://github.com/cucumber/cucumber-js/blob/main/docs/configuration.md#files
 export default {
   paths: ['./**/*.feature'],
   loader: ['ts-node/esm'],
   import: ['./**/*.test.ts'],
-
 }
