6a16a835b28598ab6937c6135a0b75245b16c78c
hibohiboo <hibohiboo66+github@gmail.com>
2024-11-15 01:54:19 +0900

    playwright導入 #7

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -2,7 +2,25 @@ import customConfig from '@odyssage/eslint-config-custom/frontend.js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config({
-  extends: [...customConfig],
+  extends: [
+    ...customConfig,
+    {
+      files: ['**/tests/**','e2e/**'],
+      rules: {
+        'import/extensions': ['off'],
+        'import/no-extraneous-dependencies': ['off'],
+        'import/no-unresolved': ['off'],
+        'sonarjs/slow-regex': ['off'],
+        'lintsonarjs/no-empty-test-file': ['off'],
+      },
+    },
+    {
+      files: ['**/e2e/**'],
+      rules: {
+        'lintsonarjs/no-empty-test-file': ['off'],
+      },
+    },
+  ],
 
   settings: {
     'import/resolver': {
@@ -10,7 +28,7 @@ export default tseslint.config({
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
       typescript: {
-        project: "./tsconfig.app.json",
+        project: './tsconfig.app.json',
       },
     },
   },
