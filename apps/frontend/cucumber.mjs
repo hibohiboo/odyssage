bcd025faa0e66fc63690c76d42a29d62ea1b3569
hibohiboo <hibohiboo66+github@gmail.com>
2024-11-16 01:52:52 +0900

    fix js #7

--- apps/frontend/cucumber.mjs
+++ apps/frontend/cucumber.mjs
@@ -1,6 +1,7 @@
 
 export default {
   paths: ['**/features/*.feature'],
-  require: ['**/tests/*.steps.ts'],
-  requireModule : ['ts-node/register']
+  import: ['**/tests/*.steps.mjs'],
+
+  
 };
---
+++ apps/frontend/e2e/tests/characterList.steps.mjs
@@ -0,0 +1,52 @@
+import { Given, When, Then } from '@cucumber/cucumber';
+import { expect, chromium } from '@playwright/test';
+
+let page;
+
+Given('アプリが起動している', async () => {
+  const browser = await chromium.launch({ headless: true }); // headless: true にするとブラウザが表示されない
+  const context = await browser.newContext();
+  page = await context.newPage();
+  await page.goto('http://localhost:5173');
+});
+
+Given('キャラクターリストが空である', async () => {
+  await page.evaluate(() => localStorage.clear());
+  await page.reload();
+});
+
+When('キャラクターリストページを表示する', async () => {
+  await page.click('text=キャラクターリスト');
+});
+
+Then('"キャラクターが見つかりませんでした" と表示される', async () => {
+  const message = await page.locator('text=キャラクターが見つかりませんでした');
+  await expect(message).toBeVisible();
+});
+
+When('{string} という名前のキャラクターを追加する', async (name) => {
+  await page.fill('input[placeholder="キャラクター名を入力"]', name);
+  await page.click('button:has-text("キャラクターを追加")');
+});
+
+Then('キャラクターリストに {string} が表示される', async (name) => {
+  const character = await page.locator(`text=${name}`);
+  await expect(character).toBeVisible();
+});
+
+When('以下のキャラクターを追加する:', async (table) => {
+  for (const { name } of table.hashes()) {
+    await page.fill('input[placeholder="キャラクター名を入力"]', name);
+    await page.click('button:has-text("キャラクターを追加")');
+  }
+});
+
+Then(
+  'キャラクターリストに {string} と {string} が表示される',
+  async (name1, name2) => {
+    const character1 = await page.locator(`text=${name1}`);
+    const character2 = await page.locator(`text=${name2}`);
+    await expect(character1).toBeVisible();
+    await expect(character2).toBeVisible();
+  },
+);
