ec7d54d34079d019d3e7f748311af3dff860ed19
hibohiboo <hibohiboo66+github@gmail.com>
2024-11-16 10:42:28 +0900

    fix ディレクトリ分割 #7

--- apps/frontend/cucumber.mjs
+++
@@ -1,7 +0,0 @@
-
-export default {
-  paths: ['**/features/*.feature'],
-  import: ['**/tests/*.steps.mjs'],
-
-  
-};
---
+++ packages/bdd-e2e-test/cucumber.mjs
@@ -0,0 +1,6 @@
+
+export default {
+  paths: ['**/features/*.feature'],
+  import: ['**/tests/*.steps.ts'],
+  loader: ['ts-node/esm']
+};
