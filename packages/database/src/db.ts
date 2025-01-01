e76ebfa152a1fb2a62fe659e3607eabc6834ca2c
hibohiboo <hibohiboo66+github@gmail.com>
2025-01-01 23:38:19 +0900

    fix drizzle 導入 #19

--- packages/database/src/db.ts
+++ packages/database/src/db.ts
@@ -1,10 +1,21 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
+import type { NeonQueryFunction } from '@neondatabase/serverless';
+import type { NeonHttpDatabase } from 'drizzle-orm/neon-http';
+
+type NeonDBClient = NeonHttpDatabase<Record<string, never>> & {
+  $client: NeonQueryFunction<false, false>;
+};
+
+let db: NeonDBClient | null = null;
 
 export const getDb = (
   connectionString = process.env.NEON_CONNECTION_STRING!,
 ) => {
+  if (db != null) {
+    return db;
+  }
   const sql = neon(connectionString);
-  const db = drizzle({ client: sql });
+  db = drizzle({ client: sql });
   return db;
 };
