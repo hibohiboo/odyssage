1a908572d106ecb57f7e5ea460073beb13d85cf8
hibohiboo <hibohiboo66+github@gmail.com>
2025-01-04 00:17:58 +0900

    fix ユーザのファイルを分割 #25

---
+++ apps/backend/src/route/index.ts
@@ -0,0 +1,57 @@
+import { getScenarios } from '@odyssage/database/src/queries/select';
+import { scenarioResponseSchema } from '@odyssage/schema/src/schema';
+import { Hono } from 'hono';
+import { describeRoute } from 'hono-openapi';
+import { resolver } from 'hono-openapi/valibot';
+import { user } from './user';
+import type { Neo4jError } from 'neo4j-driver-core';
+
+const route = new Hono<Env>()
+	.get('/', (c) => c.text('Hello Cloudflare Workers!'))
+	.route('/user', user)
+	.get(
+		'/scenarios',
+		describeRoute({
+			description: 'シナリオの一覧を取得',
+			responses: {
+				200: {
+					description: 'Successful response',
+					content: {
+						'application/json': { schema: resolver(scenarioResponseSchema) },
+					},
+				},
+			},
+		}),
+		async (c) => {
+			const data = await getScenarios(c.env.NEON_CONNECTION_STRING);
+
+			return c.json(data);
+		},
+	)
+	.get('/graph-scenarios', async (c) => {
+		// vitestが Error: No such module "node:os". というエラーを出すので、いったん動的importで逃げる
+		const neo4j = await import('neo4j-driver');
+		let driver;
+		try {
+			console.log(c.env);
+			driver = neo4j.driver(c.env.NEO4J_URL, neo4j.auth.basic(c.env.NEO4J_USER, c.env.NEO4J_PASSWORD));
+			const serverInfo = await driver.getServerInfo();
+			console.log('Connection established');
+			console.log(serverInfo);
+			const session = driver.session();
+			const result = await session.run('match (s:Scenario) return (s)');
+			const data = result.records.map((record) => record.get('s').properties);
+
+			return c.json(data);
+		} catch (err) {
+			const neo4jError = err as Neo4jError;
+			console.log(`Connection error\n${err}\nCause: ${neo4jError.cause}`);
+
+			return c.text('Server Error', 500);
+		} finally {
+			if (driver) {
+				await driver.close();
+			}
+		}
+	});
+export default route;
---
+++ apps/backend/src/route/user.ts
@@ -0,0 +1,49 @@
+import { upsertUser } from '@odyssage/database/src/queries/insert';
+import { getUserById } from '@odyssage/database/src/queries/select';
+import { userParamSchema, userSchema } from '@odyssage/schema/src/schema';
+import { Hono } from 'hono';
+import { describeRoute } from 'hono-openapi';
+import { resolver, validator as vValidator } from 'hono-openapi/valibot';
+
+export const user = new Hono<Env>()
+	.get(
+		'/:id',
+		describeRoute({
+			description: '指定したユーザを取得',
+			responses: {
+				200: {
+					description: 'Successful response',
+					content: {
+						'application/json': { schema: resolver(userSchema) },
+					},
+				},
+			},
+		}),
+		vValidator('param', userParamSchema),
+		async (c) => {
+			const param = c.req.valid('param');
+			const [data] = await getUserById(c.env.NEON_CONNECTION_STRING, param.id);
+			if (!data) {
+				return c.text('Not Found', 404);
+			}
+			return c.json(data);
+		},
+	)
+	.put(
+		'/:id',
+		describeRoute({
+			description: '指定したユーザを登録',
+			responses: {
+				204: {
+					description: 'Successful response',
+				},
+			},
+		}),
+		vValidator('param', userParamSchema),
+		async (c) => {
+			const param = c.req.valid('param');
+			await upsertUser(c.env.NEON_CONNECTION_STRING, { id: param.id });
+
+			return c.body(null, 204);
+		},
+	);
