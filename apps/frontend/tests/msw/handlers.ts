bafb6886163f8ab8a5f048e0c5d7b1f8a9611a4a
hibohiboo <hibohiboo66+github@gmail.com>
2025-01-05 22:41:26 +0900

    add msw #33

--- apps/frontend/tests/msw/handlers.ts
+++ apps/frontend/tests/msw/handlers.ts
@@ -1,24 +1,9 @@
 import { HttpResponse, http } from 'msw';
+import { userHandlers } from './responses/user';
+import { baseUrl, okResponseStatus } from './utils';
 
-const baseUrl = `${import.meta.env.VITE_BACKEND_DOMAIN}/api`;
-const okResponseStatus = {
-  status: 200,
-  headers: { 'Content-Type': 'application/json' },
-};
 export const handlers = [
-  http.get(
-    `${baseUrl}/user/:uid`,
-    () =>
-      new HttpResponse(
-        JSON.stringify([
-          {
-            id: 'test-aaaa-bbbb-cccc-29ddfc9efee1',
-          },
-        ]),
-        okResponseStatus,
-      ),
-  ),
-  http.put(`${baseUrl}/user/:uid`, () => new HttpResponse('', { status: 204 })),
+  ...userHandlers,
   http.get(
     `${baseUrl}/scenarios`,
     () =>
---
+++ apps/frontend/tests/msw/responses/user.ts
@@ -0,0 +1,18 @@
+import { http, HttpResponse } from 'msw';
+import { baseUrl, okResponseStatus } from '../utils';
+
+export const userHandlers = [
+  http.get(
+    `${baseUrl}/user/:uid`,
+    () =>
+      new HttpResponse(
+        JSON.stringify([
+          {
+            id: 'test-aaaa-bbbb-cccc-29ddfc9efee1',
+          },
+        ]),
+        okResponseStatus,
+      ),
+  ),
+  http.put(`${baseUrl}/user/:uid`, () => new HttpResponse('', { status: 204 })),
+];
