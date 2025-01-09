3720490a212e7c95d3f1aad667372e15fa33e197
hibohiboo <hibohiboo66+github@gmail.com>
2025-01-09 22:03:18 +0900

    add サインアップのテスト #38

--- packages/bdd-e2e-test/e2e/playwright/signup.test.ts
+++ packages/bdd-e2e-test/e2e/playwright/signup.test.ts
@@ -2,14 +2,16 @@ import { chromium, test, expect } from '@playwright/test';
 import type { Page } from '@playwright/test';
 
 test.beforeAll(async () => {
-  const browser = await chromium.launch({ headless: false }); // headless: true にするとブラウザが表示されない
+  const browser = await chromium.launch({ headless: true }); // headless: true にするとブラウザが表示されない
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto('http://127.0.0.1:4000/');
   await page.getByRole('link', { name: 'Go to auth emulator' }).click();
-  await page.getByLabel('Open menu for user testuser').click();
-  await page.getByRole('menuitem', { name: 'Delete user' }).click();
-  await page.getByRole('button', { name: 'Delete' }).click();
+  if (await page.isVisible("text='testuser'")) {
+    await page.getByLabel('Open menu for user testuser').click();
+    await page.getByRole('menuitem', { name: 'Delete user' }).click();
+    await page.getByRole('button', { name: 'Delete' }).click();
+  }
 });
 test('サインアップする', async ({ page }) => {
   await page.goto('http://localhost:5173/');
