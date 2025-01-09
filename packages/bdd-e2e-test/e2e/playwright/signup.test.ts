3578a073935ac31d0bf3507dc06a72b2a81a5ba9
hibohiboo <hibohiboo66+github@gmail.com>
2025-01-09 21:59:42 +0900

    fix test ok #38

--- packages/bdd-e2e-test/e2e/playwright/signup.test.ts
+++ packages/bdd-e2e-test/e2e/playwright/signup.test.ts
@@ -1,18 +1,15 @@
 import { chromium, test, expect } from '@playwright/test';
 import type { Page } from '@playwright/test';
 
-let page: Page;
-
 test.beforeAll(async () => {
   const browser = await chromium.launch({ headless: false }); // headless: true にするとブラウザが表示されない
   const context = await browser.newContext();
-  page = await context.newPage();
+  const page = await context.newPage();
   await page.goto('http://127.0.0.1:4000/');
   await page.getByRole('link', { name: 'Go to auth emulator' }).click();
   await page.getByLabel('Open menu for user testuser').click();
   await page.getByRole('menuitem', { name: 'Delete user' }).click();
   await page.getByRole('button', { name: 'Delete' }).click();
-  await page.close();
 });
 test('サインアップする', async ({ page }) => {
   await page.goto('http://localhost:5173/');
