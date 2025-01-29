b1b2899eec9bcace51bbc4396e2c9d3b600f828f
hibohiboo <hibohiboo66+github@gmail.com>
2025-01-29 23:26:30 +0900

    fix キャラクターと不要になったコアメソッドを削除 #41

--- apps/frontend/src/entities/character/model/character.factory.ts
+++
@@ -1,8 +0,0 @@
-import { Character } from '@odyssage/core/character/domain/Character';
-
-export const createCharacter = (name: string, tags: string[]): Character =>
-  new Character({
-    id: crypto.randomUUID(),
-    name,
-    tags: tags.map((tag) => ({ name: tag })),
-  });
