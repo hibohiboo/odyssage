8e79de372f9fa753990b193bab38f7fae9a5256b
hibohiboo <hibohiboo66+github@gmail.com>
2025-01-29 23:29:55 +0900

    fix 痕跡 #41

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,14 +1,13 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@odyssage/frontend': path.join(__dirname, './src'),
-      '@odyssage/core': path.join(__dirname, '../../packages/core'),
     },
   },
 });
