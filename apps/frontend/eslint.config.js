ac60a1693897232aa669b04aff94209bc4930f1f
hibohiboo <hibohiboo66+github@gmail.com>
2025-03-22 02:23:10 +0900

    refactor: enhance ESLint configuration by adding ignored files and improving structure

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -1,44 +1,45 @@
 import customConfig from '@odyssage/eslint-config-custom/frontend.js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-  ignores: ['vite.config.ts'],
-  extends: [
-    ...customConfig,
-    {
-      files: ['**/tests/**'],
-      rules: {
-        'import/extensions': ['off'],
-        'import/no-extraneous-dependencies': ['off'],
-        'import/no-unresolved': ['off'],
-        'sonarjs/slow-regex': ['off'],
-        'lintsonarjs/no-empty-test-file': ['off'],
-        'no-restricted-syntax': ['off'],
-        'no-await-in-loop': ['off'],
+export default tseslint.config(
+  { ignores: ['vite.config.ts', '**/dist/**', '**/public/**'] },
+  {
+    extends: [
+      ...customConfig,
+      {
+        files: ['**/tests/**'],
+        rules: {
+          'import/extensions': ['off'],
+          'import/no-extraneous-dependencies': ['off'],
+          'import/no-unresolved': ['off'],
+          'sonarjs/slow-regex': ['off'],
+          'lintsonarjs/no-empty-test-file': ['off'],
+          'no-restricted-syntax': ['off'],
+          'no-await-in-loop': ['off'],
+        },
       },
-    },
-    {
-      files: ['src/shared/lib/auth/**'],
-      rules: {
-        '@conarti/feature-sliced/layers-slices': ['off']
+      {
+        files: ['src/shared/lib/auth/**'],
+        rules: {
+          '@conarti/feature-sliced/layers-slices': ['off'],
+        },
       },
-    },
-    {
-      files: ['**/src/**/**Slice.ts'],
-      rules: {
-        'no-param-reassign': ['off']
+      {
+        files: ['**/src/**/**Slice.ts'],
+        rules: {
+          'no-param-reassign': ['off'],
+        },
       },
-    }
-  ],
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-      typescript: {
-        project: './tsconfig.app.json',
+    ],
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+        typescript: {
+          project: './tsconfig.app.json',
+        },
       },
     },
   },
-  
-});
+);
