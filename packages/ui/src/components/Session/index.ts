cd24c4e80184c49ce6eec839339fc0b6af943271
hibohiboo <hibohiboo66+github@gmail.com>
2025-04-03 08:51:45 +0900

    refactor: remove unused Session components and update exports #65

--- packages/ui/src/components/Session/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './ScenarioSelector';
-export * from './SessionForm';
-export * from './SessionSidebar';
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,4 +2,3 @@
 export * from './components/Button';
 export * from './components/FeatureCard';
 export * from './components/CtaButton';
-export * from './components/Session';
