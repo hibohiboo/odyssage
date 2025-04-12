83fa82e9f888b9b138f44c32050e0bb6e1a6af66
hibohiboo <hibohiboo66+github@gmail.com>
2025-04-12 18:15:18 +0900

    refactor: add screenshot capture in scenario step and update .gitignore for screenshots #75

--- packages/bdd-e2e-test/e2e/step-definitions/scenario.steps.ts
+++ packages/bdd-e2e-test/e2e/step-definitions/scenario.steps.ts
@@ -17,6 +17,8 @@ When(
   'ユーザーが「 {string} 」リンクをクリックする',
   async function (this, text) {
     const { page } = this;
+
+    await page.screenshot({ path: 'screenshots/debug.png', fullPage: true });
     await page.waitForSelector(`a:has-text("${text}")`);
     await page.getByRole('link', { name: text }).nth(0).click();
   },
