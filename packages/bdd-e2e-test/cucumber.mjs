3cde42be9e2a2d5f54f214a936dcc6a1721b876f
hibohiboo <hibohiboo66+github@gmail.com>
2025-04-15 08:21:24 +0900

    refactor: update pipeline trigger branch to develop

--- packages/bdd-e2e-test/cucumber.mjs
+++ packages/bdd-e2e-test/cucumber.mjs
@@ -1,7 +1,11 @@
 
 export default {
   // paths: ['**/features/*.feature'],
-  paths: ['**/features/scenario-create.feature'],
+  paths: [
+    process.env.CI === 'true' 
+    ? '**/features/*.feature' 
+    : '**/features/scenario-create.feature'
+  ],
   import: ['**/step-definitions/*.steps.ts'],
   loader: ['ts-node/esm']
 };
