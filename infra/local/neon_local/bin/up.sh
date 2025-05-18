f2b868b7fe171e3362e3a9e55d52dd5b1898dfc5
hibohiboo <hibohiboo66+github@gmail.com>
2025-05-19 08:54:36 +0900

    chore: remove dbdoc folder cleanup from up.sh #91

--- infra/local/neon_local/bin/up.sh
+++ infra/local/neon_local/bin/up.sh
@@ -6,8 +6,6 @@ docker_dir=$(cd $bin_dir/.. && pwd)
 # up.sh docker-compose.camp.yml
 composeFile=${1:-"docker-compose.yml"}
 
-# tblsのためにdbdocフォルダを削除
-rm -rf $docker_dir/dbdoc
 
 # docker-composeの起動
 cd $docker_dir && docker-compose --profile dev --env-file .env -f $composeFile up
