688432dd0acb4c1914b09812674a7249940eda2f
hibohiboo <hibohiboo66+github@gmail.com>
2025-06-30 22:49:52 +0900

    refactor: simplify eslint configuration structure

--- apps/backend/eslint.config.js
+++ apps/backend/eslint.config.js
@@ -1,27 +1,27 @@
 import customConfig from '@odyssage/eslint-config-custom/backend.js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-  ignores: [...customConfig[0].ignores, '**/.wrangler/**'],
-  extends: [
-    ...customConfig,
-    {
-      files: ['**/test/**'],
-      rules: {
-        'no-undef': ['off'],
-        'import/extensions': ['off'],
-        'import/no-extraneous-dependencies': ['off'],
-        'import/no-unresolved': ['off'],
-        'sonarjs/slow-regex': ['off'],
-        '@typescript-eslint/no-explicit-any': ['off'],
-      },
+export default tseslint.config(
+  customConfig,
+  {
+    ignores: [...customConfig[0].ignores, '**/.wrangler/**'],
+  },
+  {
+    files: ['**/test/**'],
+    rules: {
+      'no-undef': ['off'],
+      'import/extensions': ['off'],
+      'import/no-extraneous-dependencies': ['off'],
+      'import/no-unresolved': ['off'],
+      'sonarjs/slow-regex': ['off'],
+      '@typescript-eslint/no-explicit-any': ['off'],
     },
-    {
-      files: ['*.mts'],
-      rules: {
-        'no-undef': ['off'],
-        'import/no-extraneous-dependencies': ['off'],
-      },
+  },
+  {
+    files: ['*.mts'],
+    rules: {
+      'no-undef': ['off'],
+      'import/no-extraneous-dependencies': ['off'],
     },
-  ],
-});
+  },
+);
