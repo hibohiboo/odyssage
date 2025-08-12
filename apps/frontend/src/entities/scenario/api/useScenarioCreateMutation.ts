7339d611cbeae0723957d6487d9488aad8e388c6
hibohiboo <hibohiboo66+github@gmail.com>
2025-08-12 18:21:15 +0900

    fix フロントエンド修正 #112

--- apps/frontend/src/entities/scenario/api/useScenarioCreateMutation.ts
+++ apps/frontend/src/entities/scenario/api/useScenarioCreateMutation.ts
@@ -6,7 +6,7 @@ type Props = {
   uid: string;
 };
 export const useScenarioCreateMutation = (props: Props) => {
-  const { $post } = apiClient.api.users[':uid'].scenario;
+  const { $post } = apiClient.api.authors[':uid'].scenarios;
   const fetcherFactory =
     (param: InferRequestType<typeof $post>['param']) =>
     async (
